88840fef2a776abe33ead7e05b6c3c095c3aaa96
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-04-04 22:18:13 +0530

    Update T[] to List<T> code

--- src/core/java/practice/CoreJavaPractice.java
+++ src/core/java/practice/CoreJavaPractice.java
@@ -1,10 +1,32 @@
 package core.java.practice;
 
+import java.util.*;
+
 public class CoreJavaPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Array To List 
+		String[] arr = { "1", "2", "3"};
+		List<String> list = new ArrayList<>();
+		list = Arrays.asList(arr);
+		printList(list);
+		list = new ArrayList<>();
+		Collections.addAll(list, arr);
+		printList(list);
+	}
 
+	private static void printList(List<String> list) {
+		// TODO Auto-generated method stub
+		for(String s : list) {
+			print(s);
+		}
+	}
+
+	private static <T> void print(T genericVariable) {
+		// TODO Auto-generated method stub
+		System.out.print(genericVariable);
 	}
 
 }
